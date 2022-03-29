abab744a822132b37c329e3778d3aa0a1709bb26
bongoman-by <bongoman@tut.by>
2022-03-29 10:49:26 +0300

    feat(app): add API for MongoDB
    Closes #4

--- backend/app.js
+++ backend/app.js
@@ -13,19 +13,86 @@ const MongoDb = process.env.MongoDb;
 app.use(express.json());
 app.use(morgan('tiny'));
 
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: String,
+  richDescription: String,
+  countInStock: Number,
+  image: String,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
 // call http://localhost:3000/api/v1/products
-app.get(`${api}products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'hare dresser',
-    image: 'some_url',
-  };
-  res.send(product);
+app.get(`${api}products`, async (req, res) => {
+  try {
+    const productList = await Product.find();
+    res.status(200).json(productList);
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+      ok: false,
+    });
+  }
 });
 
 app.post(`${api}products`, (req, res) => {
-  const product = req.body;
-  res.send(product);
+  const { name, richDescription, countInStock, image } = req.body;
+  const product = new Product({
+    name,
+    richDescription,
+    countInStock: +countInStock,
+    image,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        ok: false,
+      });
+    });
+});
+
+app.delete(`${api}products`, (req, res) => {
+  const id = req.body.id;
+  Product.deleteOne({ _id: id })
+    .then((deletedProduct) => {
+      res.status(201).json(deletedProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        ok: false,
+      });
+    });
+});
+
+app.put(`${api}products`, (req, res) => {
+  const { id, name, richDescription, countInStock, image } = req.body;
+  Product.updateOne(
+    { _id: id },
+    {
+      name,
+      richDescription,
+      countInStock: +countInStock,
+      image,
+    }
+  )
+    .then((updatedProduct) => {
+      res.status(201).json(updatedProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        ok: false,
+      });
+    });
 });
 
 app.get('/', (req, res) => {
